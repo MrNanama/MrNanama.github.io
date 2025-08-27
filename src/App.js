37b24db851065b61ca73bfdad871bf175cff7490
Durukan Demir <durukandemir07@gmail.com>
2025-08-27 23:06:09 +0300

    a

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />}/>
-            <Route path="create" element={<Create />}/>
+            <Route path="/create" element={<Create />}/>
           </Routes>
         </div>
       </div>
